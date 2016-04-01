94e8584fd81f67b426780814fca87c3b538768ef
Nagato <bouayadd.n4@gmail.com>
2016-04-01 17:05:52 +0100

    dont know what i did but it scall good

--- app/src/main/java/com/rfid/mascir/hello/History.java
+++ app/src/main/java/com/rfid/mascir/hello/History.java
@@ -1,67 +1,45 @@
 package com.rfid.mascir.hello;
 
 import android.app.AlertDialog;
-import android.content.Context;
 import android.content.Intent;
 import android.database.Cursor;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.NavigationView;
 import android.support.design.widget.Snackbar;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
-import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.text.Editable;
-import android.util.Log;
-import android.view.View;
+import android.text.method.ScrollingMovementMethod;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View.OnClickListener;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.LinearLayout;
+import android.view.View;
 import android.widget.TextView;
-import android.widget.Toast;
 
-import java.io.Console;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.OutputStream;
-import java.util.Arrays;
 import java.util.Date;
 
-public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+/**
+ * Created by mac1 on 4/1/16.
+ */
+public class History extends AppCompatActivity {
 
-    TextView  textView;
-    Button Valid;
-    Button history;
-    Button test;
-    EditText editText;
-
-    String tagId;
-    //create the Tag
-
-    GPSTracker gps;
-
-    Tag tag = new Tag();
-    Date date = new Date() ;
-    DbHandler dbHandler = new DbHandler(this,null,null,1);
+    DbHandler dbHandler = new DbHandler(this, null, null, 1);
+    Date date = new Date();
+    TextView history;
 
 
-    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.historylayout);
+        history = (TextView) findViewById(R.id.history_TextView);
+        history.setMovementMethod(new ScrollingMovementMethod()); // make it scrollball
+
 
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
+        history();
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new OnClickListener() {
+        fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Snackbar.make(view, "Sending email ...", Snackbar.LENGTH_LONG)
@@ -95,89 +73,36 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
 
             }
         });
-
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
-                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
-        drawer.setDrawerListener(toggle);
-        toggle.syncState();
-
-        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
-        navigationView.setNavigationItemSelectedListener(this);
-
-        textView =  (TextView) findViewById(R.id.textView);
-        Valid = (Button) findViewById(R.id.button);
-        editText = (EditText) findViewById(R.id.editText);
-
-        gps = new GPSTracker(MainActivity.this);
-
-
-       //s watch.setText(buffer);
-
-
     }
 
+    public void dialogShow(String title, String message) {
 
-
-    /* (non-Javadoc)
-  * @see android.app.Activity#onStart()
-  */
-        @Override
-    protected void onStart() {
-
-        super.onStart();
-        valid();
-    }
-
-
-
-    public void valid(){
-        Valid.setOnClickListener(new OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-                tagId = String.valueOf(editText.getText());
-                tag.setTagId(tagId);
-                tag.setDate(String.valueOf(date));
-
-                if (gps.canGetLocation()) {
-                    double latitude = gps.getLatitude();
-                    double longitude = gps.getLongitude();
-                    tag.setLatitude(latitude);
-                    tag.setLongitude(longitude);
-
-                    Toast.makeText(
-                            getApplicationContext(),
-                            "Your Location is -\nLat: " + latitude + "\nLong: "
-                                    + longitude, Toast.LENGTH_LONG).show();
-                } else {
-                    gps.showSettingsAlert();
-                }
-                ////////////////////////////////////////////////
-                dbHandler.addTag(tag);
-                Snackbar.make(v, "Tag Serial saved", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-                //editText.setText(" ");
-            }
-        });
-    }
-
-
-
-    public void dialogShow(String title,String message){
-
-        AlertDialog.Builder builder = new AlertDialog.Builder(this );
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
         builder.setCancelable(true);
         builder.setTitle(title);
         builder.setMessage(message);
         builder.show();
     }
 
-    protected void OnDestroy(){
-        super.onDestroy();
-        dbHandler.close();
-        System.out.println("deconnection done");
+    public void history (){
+
+                Cursor c = dbHandler.db2string();
+                if (c.getCount() == 0) {
+                    //watch.setText("NO DATA AVAILIBALE");
+                    dialogShow("Error", "NO DATA AVAILIBALE");
+                    return;
+                }
+                StringBuffer buffer = new StringBuffer();
+                while (c.moveToNext()) {
+                    buffer.append("ID :" + c.getString(0) + "\n");
+                    buffer.append("TagID :" + c.getString(1) + "\n");
+                    buffer.append("DATE :" + c.getString(2) + "\n");
+                    buffer.append("Lat :" + c.getString(3) + " | ");
+                    buffer.append("Long :" + c.getString(4) + "\n\n");
+                }
+                history.setText(buffer.toString());
     }
+
 
     public void onBackPressed() {
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
@@ -191,7 +116,7 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
+        getMenuInflater().inflate(R.menu.main_history, menu);
         return true;
     }
 
@@ -215,7 +140,6 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
     }
 
     @SuppressWarnings("StatementWithEmptyBody")
-    @Override
     public boolean onNavigationItemSelected(MenuItem item) {
         // Handle navigation view item clicks here.
         dialogShow("on","Handle navigation view item clicks here.");
@@ -242,7 +166,12 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         drawer.closeDrawer(GravityCompat.START);
         return true;
     }
-}
 
+    protected void OnDestroy(){
+        super.onDestroy();
+        dbHandler.close();
+    }
+
+}
 
 
